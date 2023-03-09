fde001214cd5a0a36d5bcfa58642175796d1bf51
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2023-03-09 21:42:24 +0530

    New

--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -5,6 +5,7 @@ function Textarea() {
    <>
    <div className="container form-floating my-5">
   <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
+  <button className='btn btn-dark my-3'>Convert to upper case</button>
   </div>
    </>
   )
