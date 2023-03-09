bc1fdabf1cd095d042bd21fda4752bdd9806c928
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2023-03-09 21:38:50 +0530

    Second

---
+++ src/Components/Textarea.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Textarea() {
+  return (
+   <>
+   <div className="container form-floating my-5">
+  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
+  </div>
+   </>
+  )
+}
+
+export default Textarea
