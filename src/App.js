8fd2eb101bc7742ae6e9e8440dc3f1c76009660e
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2023-03-10 21:37:15 +0530

    Nav updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from 'react';
 import Navbar from "./Components/Navbar"
 import Textarea from "./Components/Textarea";
+
 // import About from "./Components/About";
 
 
 function App() {
+
+  const [mode,setMode]=useState('light');
+
+const toggleMode =()=>{
+  if(mode==='light'){
+    setMode('dark')
+  }else{
+    setMode('light')
+  }
+}
   return (
     <div className="App">
-      <Navbar title="Titly" ></Navbar>
+      < Navbar title="Titly" mode={mode} toggleMode={toggleMode}/>
       <Textarea heading= "Text Modification App"></Textarea>
 
     
