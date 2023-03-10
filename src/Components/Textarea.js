810111c6f9fbfd0256bf0e0d1fc73ae64418bdd2
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2023-03-10 20:58:32 +0530

    App updated

--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -4,7 +4,7 @@ import React,{useState} from 'react'
 function Textarea(props) {
 
     const handleUppercase =()=>{
-      const newText = text.toUpperCase() ;
+      let newText = text.toUpperCase() ;
       setText(newText);
         
     }
@@ -20,6 +20,22 @@ function Textarea(props) {
       setText(newText3)
     }
 
+    const handleCopyText=()=>{
+      let text= document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value) ;
+
+    }
+
+    const handleSpaces=()=>{
+      let newText = text.split(/[ ]+/)
+      setText(newText.join(" "))
+    }
+
+    const handleFonts=()=>{
+      let newtext = document.getElementById('myBox') ;
+      newtext.style.fontFamily="Cursive"
+    }
 
 
 
@@ -52,12 +68,15 @@ function Textarea(props) {
    <>
    <div className="container form-floating my-5">
    <h1>{props.heading}</h1>
-  <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Leave a comment here" id="floatingTextarea2"  ></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Leave a comment here" id="myBox"  ></textarea>
   <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleUppercase} >Convert to upper case</button>
 
   <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleLowercase} >Convert to lower case</button>
   <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleClearClick} >Clear Text</button>
- 
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleCopyText} >Copy Text</button>
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleSpaces} >Remove spaces</button>
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleFonts} >Change Fonts</button>
+
 
 
 
