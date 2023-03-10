fbf3a18a8056a7afa2832bc27a73513559943674
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2023-03-10 20:30:49 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Navbar from "./Components/Navbar"
 import Textarea from "./Components/Textarea";
+// import About from "./Components/About";
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar title="Titly"></Navbar>
-      <Textarea></Textarea>
-      
+      <Navbar title="Titly" ></Navbar>
+      <Textarea heading= "Text Modification App"></Textarea>
+
+    
+
+  {/* <About about="Accordian"></About>*/}
+
     </div>
   );
 }
--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function Textarea() {
+
+function Textarea(props) {
+
+    const handleUppercase =()=>{
+      const newText = text.toUpperCase() ;
+      setText(newText);
+        
+    }
+
+
+    const handleLowercase=()=>{
+      const newText2 = text.toLowerCase();
+      setText(newText2)
+    }
+
+    const handleClearClick =()=>{
+      let newText3 ="" ;
+      setText(newText3)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const handleOnChange=(event)=>{
+      setText(event.target.value) ;
+
+    }
+
+
+
+
+
+    const[text,setText]=useState("")
+
+
+
   return (
    <>
    <div className="container form-floating my-5">
-  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
-  <button className='btn btn-dark my-3'>Convert to upper case</button>
+   <h1>{props.heading}</h1>
+  <textarea className="form-control" value={text} onChange={handleOnChange} placeholder="Leave a comment here" id="floatingTextarea2"  ></textarea>
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleUppercase} >Convert to upper case</button>
+
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleLowercase} >Convert to lower case</button>
+  <button className='btn btn-dark my-3 mx-1'type='button' onClick={handleClearClick} >Clear Text</button>
+ 
+
+
+
+
+  </div>
+  <div className="container my-2">
+    <h1>your text summary</h1>
+    <p>{text.split(" ").length} words, {text.length} characters</p>
+    <p>{0.008 * text.split(" ").length} minutes read</p>
   </div>
    </>
   )
